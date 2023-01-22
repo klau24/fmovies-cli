2b1571375e65f1658b6dd4b8aa9c523fb7b04fa6
Kenny Lau <klau24@calpoly.edu>
2023-01-21 20:34:57 -0800

    refactored

--- fmovies.py
+++ fmovies.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import sys
 import urllib
 import requests
 import shutil
@@ -10,43 +11,48 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-# init Chrome driver (Selenium)
-options = Options()
-options.add_experimental_option('w3c', False) ### added this line
-options.headless = True
-cap = DesiredCapabilities.CHROME
-cap["loggingPrefs"] = {"performance": "ALL"}
+def seleniumInit():
+    # init Chrome driver (Selenium)
+    options = Options()
+    options.add_experimental_option('w3c', False) ### added this line
+    options.headless = True
+    cap = DesiredCapabilities.CHROME
+    cap["loggingPrefs"] = {"performance": "ALL"}
 
-driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver",
-                              options=options,
-                              desired_capabilities=cap)
+    driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options, desired_capabilities=cap)
+    return drive
 
+def downloadMovie(driver):
+    if len(sys.argv) == 1:
+        return print("Please provide a movie")
 
+    movie = sys.argv[1])
+    driver.get(movie)
+    # kinda iffy, running into some errors ocassionally here
+    driver.find_element(By.ID, "play-now").click()
+    time.sleep(3)
+    video = driver.find_element(By.TAG_NAME, "iframe").get_attribute('src')
+    print("Found video...")
+    driver.get(video)
+    time.sleep(3)
+    log = driver.get_log("performance")
+    driver.quit()
 
-driver.get("https://ww3.fmovies.co/film/interstellar-1655/")
-# kinda iff, running into some errors ocassionally here
-driver.find_element(By.ID, "playit").click()
-time.sleep(3)
-video = driver.find_element(By.TAG_NAME, "iframe").get_attribute('src')
-print("Found video...")
-driver.get(video)
-time.sleep(3)
-print("Still working...")
+    blob = ""
+    for i in log:
+        res = str(i)
+        if "master.m3u8" in res:
+            logDict = json.loads(i["message"])
+            headers = logDict["message"]["params"]["headers"]
+            blob = "https://" + headers[":authority"] + headers[":path"]
+            print("Video source: {blob}".format(blob=blob))
+            break
 
-log = driver.get_log("performance")
-driver.quit()
+    print("Starting video download...")
+    ydl_opts = {"nocheckcertificate": True, "format": "mp4", "outtmpl": "./movies/movie.mp4"}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([blob])
 
-blob = ""
-for i in log:
-    res = str(i)
-    if "master.m3u8" in res:
-        logDict = json.loads(i["message"])
-        headers = logDict["message"]["params"]["headers"]
-        blob = "https://" + headers[":authority"] + headers[":path"]
-        print(blob)
-        break
-
-print("Starting video download...")
-ydl_opts = {"nocheckcertificate": True, "format": "mp4", "outtmpl": "./interstellar.mp4"}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([blob])
+if __name__ == "__main__":
+    driver = seleniumInit()
+    downloadMovie(driver)
